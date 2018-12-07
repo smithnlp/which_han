f66323bfa3ea23acd78645fad06ffdcf922fca10
smithnlp <smithnlp@users.noreply.github.com>
2018-12-06 22:44:28 -0700

    pivot to dialect feature explanation

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # which_han
-This is a super-simple program which uses naïve linguistic features to differentiate between written Mandarin and written Cantonese, outputting a prediction of to which Han language the text belongs.
+This is a super-simple tool intended for learners of Chinese who are far along enough to *know what they don't know*. The idea is that if you come across Chinese text that you can tell is __not__ standard Mandarin Putonghua, paste it into this program and it will explain dialect characters, grammar, etc.
+
+### Han dialect support
+* Yue (Cantonese)
+* Wu (Shanghainese)
+And maybe
+* Gan
+
+### Where you might come across dialect writing
+* online forums
+* some blogs
+* there are dialect wikipedia's for [Yue](https://zh-yue.wikipedia.org/wiki/), [Wu](https://wuu.wikipedia.org/wiki), and [Gan](https://gan.wikipedia.org/wiki)
--- which_han.py
+++ which_han.py
@@ -1,9 +1,44 @@
-import re
-import sys
+"""This is a tool which tries to decipher which Han dialect peculiarities are
+present in any given string of non-standard Chinese text. It's intended users
+are learners of Chinese who know enough of the language to know what they don't
+know.
 
-intext = sys.stdin.read()
+TODO:
+- actual dialect-specific features such as characters and grammar patterns
+- explanations
+- citations
+"""
+# import re
+# import sys
 
-if re.search(r'佢', intext):
-    print("I think this is Cantonese.")
-else:
-    print("I think this is Mandarin.")
+
+def check_yue(text):
+    """See if there are Cantonese trigger-characters and/or other patterns.
+    """
+    pass
+
+
+def check_wu(text):
+    """See if there are characteristically Wu-dialect linguistic features.
+    """
+    pass
+
+
+def check_gan(text):
+    """See if there are Gan-specific features.
+    """
+    pass
+
+
+def main():
+    """Take some string of Hanzi characters submitted by the user check for
+    dialect traits.
+    """
+    text = input("Paste some non-standard Chinese text and this program will try to decipher which Han variety it represents and explain the distinguishing features.\nYOUR TEXT: ")  # noqa
+    check_yue(text)
+    check_wu(text)
+    check_gan(text)
+
+
+if __name__ == '__main__':
+    main()
